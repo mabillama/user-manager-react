5cf61792208a449a340ea6233e7054aea4669e2d
Marc Abillama <mabillama@gmail.com>
2022-01-19 10:34:06 +0100

    Basic form validation logic added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      console.log(`Failed validation step 1 (no input)`);
+      return;
+    }
+    if (+enteredAge < 1) {
+      console.log(`Failed validation step 2 (negative value)`);
+      return;
+    }
+    console.log(enteredAge, enteredUsername);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card>
       <form className={classes.input} onSubmit={addUserHandler}>
-        <label for="usernameLocal">Username</label>
-        <input id="usernameLocal" type="text" />
-        <label for="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <label htmlFor="usernameLocal">Username</label>
+        <input
+          id="usernameLocal"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add user</Button>
       </form>
     </Card>
